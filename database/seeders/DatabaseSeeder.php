8df27399de0a3e145011901dbc5845cac11e5979
Alfian Gading Saputra <alfiangadingsaputra@gmail.com>
2022-11-19 16:57:02 +0700

    add "detail unit kompetensi" seeder and seperate seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            MataPelajaranSeeder::class,
+            JenisStandarSeeder::class,
+            UnitKompetensiSeeder::class,
+            DetailUnitKompetensiSeeder::class
+        ]);
     }
 }
---
+++ database/seeders/DetailUnitKompetensiSeeder.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DetailUnitKompetensiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 1,
+            'judul' => 'Mengikuti Praktek-praktek Kerja Yang Aman',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 1,
+            'judul' => 'Melaporkan Bahaya-bahaya di Tempat Kerja',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 1,
+            'judul' => 'Mengikuti Prosedur-prosedur Darurat',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 2,
+            'judul' => 'Memahami dan Mengikuti Operasi (Kerja) yang Terstandar Maupun Persyaratan Spesifikas',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 2,
+            'judul' => 'Digunakan Dalam Perbaikan Kualitas',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 3,
+            'judul' => 'Mengidentifikasikan Kode Etik yang Berlaku di Dunia TIK',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 3,
+            'judul' => 'Mengidentifikasikan Hal-hal yang Berkaitan dengan HAKI di Dunia TIK',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 4,
+            'judul' => 'Mengidentifikasikan Rancangan User Interface',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 4,
+            'judul' => 'Melakukan Implementasi Rancangan User Interface',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 5,
+            'judul' => 'Mengidentifikasikan Mekanisme Running atau Eksekusi Source Code',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 5,
+            'judul' => 'Mengeksekusi Source Code',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 5,
+            'judul' => 'Mengidentifikasikan Hasil Eksekusi',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 6,
+            'judul' => 'Melakukan Konfigurasi Tools untuk Pemrograman',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 6,
+            'judul' => 'Menggunakan Tools Sesuai Kebutuhan Pembuatan Program',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 7,
+            'judul' => 'Membuat Program Berorientasi Objek dengan Memanfaatkan Class',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 7,
+            'judul' => 'Menggunakan Tipe Data dan Control Program pada Metode atau Operasi dari Suatu Kelas',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 7,
+            'judul' => 'Membuat Program dengan Konsep Berbasis Objek',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 7,
+            'judul' => 'Membuat Program Object Oriented dengan Interface dan Paket',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 7,
+            'judul' => 'Mengkompilasi Program',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 8,
+            'judul' => 'Mempersiapkan Perangkat Lunak Aplikasi Data Deskripsi/SQL',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 8,
+            'judul' => 'Menggunakan Fitur Aplikasi SQL',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 8,
+            'judul' => 'Mengisi Tabel',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 8,
+            'judul' => 'Melakukan Operasi Relasional',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 8,
+            'judul' => 'Membuat Stored Procedure',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 8,
+            'judul' => 'Membuat Function',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 8,
+            'judul' => 'Membuat Trigger',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 8,
+            'judul' => 'Melakukan Perintah Commit dan Rollback',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 9,
+            'judul' => 'Melakukan Identifikasi Kode Program',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 9,
+            'judul' => 'Membuat Dokumentasi Modul Program',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 9,
+            'judul' => 'Membuat Dokumentasi Fungsi, Prosedur atau Method Program',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 9,
+            'judul' => 'Men-generate Dokumentasi',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 10,
+            'judul' => 'Menjelaskan Kebutuhan Platform',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 10,
+            'judul' => 'Mempersiapkan Bahasa Pengolahan Multimedia',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 10,
+            'judul' => 'Menggunakan Syntax Khusus Multimedia',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 10,
+            'judul' => 'Menggunakan Statement',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 10,
+            'judul' => 'Menetapkan Kompatibilitas dari Hardware dan Software',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 11,
+            'judul' => 'Menetapkan Kebutuhan Uji Coba dalam Pengembangan',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 11,
+            'judul' => 'Mempersiapkan Dokumen Uji Coba',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 11,
+            'judul' => 'Mempersiapkan Data Uji',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 11,
+            'judul' => 'Melaksanakan Prosedur Uji Coba',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 11,
+            'judul' => 'Mengevaluasi Hasil Uji Coba',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 12,
+            'judul' => 'Melakukan Analisis Keberadaan dan Kebutuhan Environment',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 12,
+            'judul' => 'Melakukan Konfigurasi Perangkay Lunak Masing-masing Environment',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 13,
+            'judul' => 'Analisis Informasi Yang Perlu di Log Pada Suatu Aplikasi',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 13,
+            'judul' => 'Membuat Modul Pembuatan Log Aplikasi',
+        ]);
+        \App\Models\DetailUnitKompetensi::factory()->create([
+            'unit_kompetensi_id' => 13,
+            'judul' => 'Membuat Modul Pengambilan Data dan Informasi Log',
+        ]);
+    }
+}
